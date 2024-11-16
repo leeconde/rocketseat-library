1e442ee9438bd56459f1406d4d7e4b3d1c13dc96
leeconde <lecondesouza@gmail.com>
2024-11-16 19:09:24 -0300

    Remover codigo nao usado

--- src/main/java/org/example/exception/BookWasNotAvailableException.java
+++
@@ -1,8 +0,0 @@
-package org.example.exception;
-
-public class BookWasNotAvailableException extends RuntimeException {
-
-    public BookWasNotAvailableException() {
-        super("The book was not available");
-    }
-}
